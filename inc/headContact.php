1fddc3aa63f144103b86a0c94660e0f4e5a2afbd
budp02 <77006733+budp02@users.noreply.github.com>
2021-01-09 22:12:25 +0100

    Adding heading file

---
+++ inc/headContact.php
@@ -0,0 +1,22 @@
+<!--
+
+ * Copyright (C) 2015 - 2020 Petr Budinský
+
+ * Soubor head.php obsahuje styly a scripty pro běžné uživatele
+
+-->
+
+<head>
+    <title><?php echo $pageTitle ?>ZŠ Vraňany</title>
+    <meta charset=utf-8>
+    <meta name=description
+          content="Základní škola Vraňany v okrese Mělník je malotřídní školou pro vzdělávání dětí od 1. do 5. ročníku.">
+    <meta name=viewport content="width=device-width, initial-scale=1, maximum-scale=5">
+    <link rel="stylesheet" type="text/css" href="css/styles_min.css?v=93">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="js/menu.js?v=20"></script>
+    <link rel="stylesheet" href="css/bootstrap.min.css?v=16">
+    <link rel="stylesheet" href="css/bootstrap-reboot.min.css?v=3">
+    <link rel="shortcut icon" href=favicon.ico>
+</head>
+
